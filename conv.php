61daa1b7f3be08d4b20e236fbc6eb0943214e877
Yura Yuki <jyugoya@users.noreply.github.com>
2016-10-25 09:18:06 +0900

    書式確認といくつか要望 (#3)
    * 黒霧さん作のmdに構成を合わせてみました。
    和文漢字は項目を分け、元ネタ補足、にしてみましたが、名前の由来、の方がわかりやすいかも。
    
    Signed-off-by: Yura YUKI <jyugoya@gmail.com>
    
    * 和名が植物に限ると誤解されないように、分離して補足説明を加えました。
    
    * 新規投稿〆切を7/17 9:00から、7/16 24:00に変更しました。
    
    * 会議の内容を反映しました。
    ・タイトルについて変更
    ・説明を追加
    ・その他微修正
    
    * 連絡先にツイッター関連を追加しました＆前文を書きました。
    
    * 作戦詳細のリンクを張り替えました。
    
    * 複数投稿の場合の１投稿１キャラを明記。一般参加の国民番号記載を変更。ルールへのリンクを追加。
    あと、ちょこちょこ微修正しました。投稿時に跳ねられたので、一部リンクの冒頭hを落としてます。
    
    * 連絡先に海法さんのツイッターアカウントを追加してきました。
    
    * ２人目以降の補欠処理について追記しました。
    
    * エントリー記事のスクレイピング用スクリプトと、設定類似度チェッカーをコミット。
    Ruby2.2にて動作確認。
    
    * コメント追加
    
    * 細かい修正。do ..end とかスクレイピングのエンコードをsjis固定にとか。
    
    * コメントアウトを直した。
    
    * スプレッドシートからダウンロードしたCSVファイルをそれっぽい一覧に整形してみるだけのくそ汚いコードです
    その辺から拾ってきたサンプルコードを適当にいじっただけな突貫コードです。
    Google APIを直接叩きたくて格闘しましたが失敗に終わりました…orz
    
    * 掲示板投稿用にurlの先頭のhを落とす処理を入れました。
    ファイルアップロード時に、チェックボックスか何かで選択できるようにしようかと思いましたが、今回自分しか使わなかったので、やめました。
    
    * Game 2 version
    
    * データ管理ツールのロールアウトに伴い、既存データを登録するために書式形式で生成するようにしました。
    
    * Signed-off-by: Yura YUKI <jyugoya@gmail.com>
    とりあえずログです。

---
+++ conv.php
@@ -0,0 +1,11 @@
+<!-- データのエンコード方式である enctype は、必ず以下のようにしなければなりません -->
+<form enctype="multipart/form-data" action="textize_csv.php" method="POST">
+    <!-- 編成用オプション -->
+
+    <!-- MAX_FILE_SIZE は、必ず "file" input フィールドより前になければなりません -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+    <!-- input 要素の name 属性の値が、$_FILES 配列のキーになります -->
+    このファイルをアップロード: <input name="csvfile" type="file" />
+    <input type="submit" value="ファイルを送信" />
+</form>
+
---
+++ textize_csv.php
@@ -0,0 +1,91 @@
+<?php
+
+echo $_FILES["csvfile"]["name"]."を変換します<br/><br/>";
+
+if (is_uploaded_file($_FILES["csvfile"]["tmp_name"])) {
+
+  $file_tmp_name = $_FILES["csvfile"]["tmp_name"];
+  $file_name = $_FILES["csvfile"]["name"];
+
+  //拡張子を判定
+  if (pathinfo($file_name, PATHINFO_EXTENSION) != 'csv') {
+    $err_msg = 'CSVファイルのみ対応しています。';
+  } else {
+    //ファイルをdataディレクトリに移動
+    if (move_uploaded_file($file_tmp_name, "./data/" . $file_name)) {
+      //後で削除できるように権限を644に
+      chmod("./data/" . $file_name, 0644);
+      $msg = $file_name . "をアップロードしました。";
+      $file = './data/'.$file_name;
+      $fp   = fopen($file, "r");
+
+      //一行目は捨てる
+      fgetcsv($fp, 0, ",");
+      while (($data = fgetcsv($fp, 0, ",")) !== FALSE) {
+        
+        //echo "募集職業：";
+        //echo mb_convert_encoding ($data[1], "SJIS", "AUTO");
+        //echo "<br/>";
+
+        // 行頭のhを削除
+        // $data[4] = ltrim($data[4], 'h');
+        echo "登録記事URL：";
+        echo mb_convert_encoding ($data[4], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "登録ID：";
+        echo mb_convert_encoding ($data[2], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "キャラクター名：";
+        echo mb_convert_encoding ($data[5], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "名前の由来：";
+        echo mb_convert_encoding ($data[6], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "プレイヤー名：";
+        echo mb_convert_encoding ($data[7], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "国民番号：";
+        echo mb_convert_encoding ($data[8], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "職業名：";
+        echo mb_convert_encoding ($data[9], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "部隊内担当：";
+        echo mb_convert_encoding ($data[10], "SJIS", "AUTO");
+        echo "<br/>";
+
+        echo "設定：000文字<br/>";
+        echo mb_convert_encoding ($data[11], "SJIS", "AUTO");
+        echo "<br/>";
+
+        //echo "備考：";
+        //echo mb_convert_encoding ($data[12], "SJIS", "AUTO");
+        //echo "<br/>";
+
+        //echo "複数キャラ番目";
+        //echo mb_convert_encoding ($data[3], "SJIS", "AUTO");
+
+        echo "<br>";
+      }
+      fclose($fp);
+      //ファイルの削除
+      unlink('./data/'.$file_name);
+    } else {
+      $err_msg = "ファイルをアップロードできません。";
+    }
+  }
+} else {
+  echo "is_uploaded_fileのチェックに失敗しています<br/>";
+  $err_msg = "ファイルが選択されていません。";
+}
+
+?>
+
+
